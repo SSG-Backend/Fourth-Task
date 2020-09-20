18528b20771d36a8676041eb697152eb68e88f7b
Joshua Odejide <54546188+joshua-s2@users.noreply.github.com>
2020-09-20 21:51:38 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,64 @@
+//jshint esversion:6
+// const express = require("express");
+// const app = express();
+// const db = require("./models/index");
+// const initRoutes = require("./routes/web");
+
+// global.__basedir = __dirname;
+
+// app.use(express.urlencoded({ extended: true }));
+// initRoutes(app);
+
+// db.sequelize.sync();
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log("Drop and re-sync db.");
+// });
+
+// let port = 3000;
+// app.listen(port, () => {
+//     console.log(`Running at localhost:${port}`);
+// });
+
+
+
+// const express = require("express");
+// const app = express();
+// const db = require("./models/image.model.js");
+// const initRoutes = require("./routes/web.js");
+
+// global.__basedir = __dirname;
+
+// app.use(express.urlencoded({ extended: true }));
+// initRoutes(app);
+
+// db.sequelize.sync();
+
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log("Drop and re-sync db.");
+// });
+
+// let port = 3000;
+// app.listen(port, () => {
+//     console.log(`Running at localhost:${port}`);
+// });
+
+
+const express = require("express");
+const app = express();
+const db = require("./src/models");
+const initRoutes = require("./src/routes/web");
+
+global.__basedir = __dirname;
+
+app.use(express.urlencoded({ extended: true }));
+initRoutes(app);
+
+// db.sequelize.sync();
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+});
+
+let port = 3000;
+app.listen(port, () => {
+    console.log(`Running at localhost:${port}`);
+});
